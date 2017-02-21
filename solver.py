85990793026b23a63b44c5854205e2ccae98876f
Ryder Bergerud <ryder.bergerud@gmail.com>
2017-02-21 15:28:28 -0800

    Added condition that makes solver work
    Added condition that solver should return none if there are no candidate values for a space, allowing the node before that to consider the candidate chosen before as a dead end.

--- solver.py
+++ solver.py
@@ -9,10 +9,10 @@ def search(values):
     # Choose one of the unfilled squares with the fewest possibilities
     #Check for squares with more than one possibility (base case for recursion)
     unresolved_keys = [k for k,v in values.items() if len(v) > 1]
-    print(unresolved_keys)
-
+    # print(unresolved_keys)
+    empty_keys = [k for k,v in values.items() if len(v) == 0]
     solution = None
-    if (len(unresolved_keys) > 0):
+    if (len(unresolved_keys) > 0 and len(empty_keys) == 0):
         min_index = unresolved_keys[0] #Want to make sure this has more than one value.
         for index in unresolved_keys:
 
@@ -22,17 +22,21 @@ def search(values):
 
         candidates = list(values[min_index])
         while len(candidates) > 0 and solution == None:
+            print(candidates, min_index)
+            display(values)
             candidate_board = {k:v for k,v in values.items() }
             # pdb.set_trace()
-            print(candidate_board, values)
+            # print(candidate_board, values)
             candidate_board[min_index] = candidates.pop()
 
             solution = search(candidate_board)
+    elif (len(empty_keys) > 0):
+        solution = None
     else:
         solution = values
     return solution
 
 grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
 values = grid_values(grid2)
-search(values)
+values = search(values)
 display(values)
