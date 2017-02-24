d769af39bdc439aeb0a0bd19c5506b66b1afb51e
Ryder Bergerud <ryder.bergerud@gmail.com>
2017-02-24 14:32:04 -0800

    Added support for solving diagonal puzzle by adding diagonal units to unitlist

--- utils.py
+++ utils.py
@@ -9,7 +9,8 @@ boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+diagonal_units = [[rows[i] + cols[i] for i in range(0,9)],[rows[i] + cols[8 - i] for i in range(0,9)]]
+unitlist = row_units + col_units + square_units + diagonal_units #Array of units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
